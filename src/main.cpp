c092a77bafbbf85414d5809b5538e2bc0b4e69f6
dmillard <dmillard10@gmail.com>
2013-01-03 13:10:03 -0500

    image processing added

--- src/main.cpp
+++ src/main.cpp
@@ -1,141 +1,71 @@
+// ros
 #include "ros/ros.h"
-//#include "std_msgs/Empty.h"
-//#include "geometry_msgs/Twist.h"
+#include "sensor_msgs/Image.h"
+#include "std_msgs/Empty.h"
+#include "geometry_msgs/Twist.h"
 
-#include "cv.h"
-#include "highgui.h"
+// opencv2
+#include "sensor_msgs/image_encodings.h"
+#include "image_transport/image_transport.h"
+#include "cv_bridge/cv_bridge.h"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
 
-IplImage* GetThresholdedImage(IplImage* img) {
-    // Convert the image into an HSV image
-    IplImage* imgHSV = cvCreateImage(cvGetSize(img), 8, 3);
-    cvCvtColor(img, imgHSV, CV_BGR2HSV); 
-    IplImage* imgThreshed = cvCreateImage(cvGetSize(img), 8, 1); 
-    cvInRangeS(imgHSV, cvScalar(162, 80, 220), cvScalar(172, 250, 255), imgThreshed); 
-    cvReleaseImage(&imgHSV);
-    return imgThreshed;
-} 
+// ardrone_autonomy
+#include "ardrone_autonomy/CamChannel.h"
 
-int main() {
-    // Initialize capturing live feed from the camera
-    CvCapture* capture = 0; 
-    capture = cvCaptureFromCAM(0); 
+using namespace cv;
+using namespace std;
 
-    // Couldn't get a device? Throw an error and quit
-    if (!capture) {
-        printf("Could not initialize capturing...\n");
-        return -1; 
-    }
-
-    // The two windows we'll be usings
-    cvNamedWindow("video"); 
-    cvNamedWindow("thresh"); 
-    
-    // This image holds the "scribble data...
-    // the tracked positions of the ball
-    IplImage* imgScribble = NULL; 
-
-    // An infinite loop
-    while(true) {
-        
-        // Will hold a frame captured from the camera
-        IplImage* frame = 0; 
-        frame = cvQueryFrame(capture); 
-        
-        // If we couldn't grab a frame... quit
-        if (!frame)
-            break; 
-        
-        // If this is the first frame, we need to initialize it
-        if (imgScribble == NULL) {
-            imgScribble = cvCreateImage(cvGetSize(frame), 8, 3); 
-        }
-
-        // Holds the yellow thresholded image (yellow = white, rest = black)
-        IplImage* imgYellowThresh = GetThresholdedImage(frame); 
-
-        // Calculate the moments to estimate the position of the ball
-        CvMoments *moments = (CvMoments*)malloc(sizeof(CvMoments)); 
-        cvMoments(imgYellowThresh, moments, 1); 
-
-        // The actual moment values
-        double moment10 = cvGetSpatialMoment(moments, 1, 0); 
-        double moment01 = cvGetSpatialMoment(moments, 0, 1); 
-        double area = cvGetCentralMoment(moments, 0, 0); 
-
-        // Holding the last and current ball positions
-        static int posX = 0; 
-        static int posY = 0; 
-
-        int lastX = posX; 
-        int lastY = posY; 
-
-        posX = moment10/area;
-        posY = moment01/area; 
-
-        // Print it out for debugging purposes
-        printf("position (%d, %d)\n", posX, posY); 
-
-        // We want to draw a line only if it's a valid position
-        if(lastX>0 && lastY>0 && posX>0 && posY>0) {
-            // Draw a yellow line from the previous point to the current point
-            cvLine(imgScribble, cvPoint(posX, posY), cvPoint(lastX, lastY), cvScalar(0,255,255), 5);
-        }
-
-        // Add the scribbling image and the frame...
-        cvAdd(frame, imgScribble, frame);
-        cvShowImage("thresh", imgYellowThresh);
-        cvShowImage("video", frame);
-
-        // Wait for a keypress
-        int c = cvWaitKey(10);
-        if(c!=-1) {
-            // If pressed, break out of the loop
-            break;
-        }
-
-         // Release the thresholded image+moments... we need no memory leaks.. please
-        cvReleaseImage(&imgYellowThresh);
-        delete moments;
-    }
-
-    // We're done using the camera. Other applications can now use it
-    cvReleaseCapture(&capture);
-    return 0;
+// threshold images and adjust drone position accordingly
+void CamCallback(const sensor_msgs::ImageConstPtr& rosimg) {
+    cv_bridge::CvImageConstPtr cvimg = cv_bridge::toCvShare(rosimg);
+    Mat cvimghsv; Mat cvimgthresh;
+    cvtColor(cvimg->image, cvimghsv, CV_RGB2HSV);
+    inRange(cvimghsv,
+            Scalar(0, 0, 240),
+            Scalar(180, 255, 255),
+            cvimgthresh);
+    Moments m = moments(cvimgthresh);
+    double x = m.m10/m.m00;
+    double y = m.m01/m.m00;
+    double xp = x/cvimgthresh.size.p[1];
+    double yp = y/cvimgthresh.size.p[0];
+    ROS_INFO("position: (%f, %f)\n", xp, yp);
 }
 
-/*
 int main(int argc, char **argv) {
+    // ros init
     ros::init(argc, argv, "thinc_main");
     ros::NodeHandle n;
-    ros::Publisher takeoff_pub = n.advertise<std_msgs::Empty>("ardrone/takeoff", 5, false);
-    ros::Publisher land_pub = n.advertise<std_msgs::Empty>("ardrone/land", 5, false);
-    ros::Publisher reset_pub = n.advertise<std_msgs::Empty>("ardrone/reset", 5, false);
-    ros::Publisher twist_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 10, false);
     ros::Rate loop_rate(10);
 
-  ros::Duration(1.0).sleep();
-    
+    // publisher, subscribers, and services
+    ros::Publisher launch = n.advertise<std_msgs::Empty>("ardrone/takeoff", 5);
+    ros::Publisher land = n.advertise<std_msgs::Empty>("ardrone/land", 5);
+    ros::Publisher reset = n.advertise<std_msgs::Empty>("ardrone/reset", 5);
+    ros::Publisher twist = n.advertise<geometry_msgs::Twist>("cmd_vel", 10);
+    ros::Subscriber cam =
+        n.subscribe<sensor_msgs::Image>("ardrone/image_raw", 1, CamCallback);
+    ros::ServiceClient camchannel =
+        n.serviceClient<ardrone_autonomy::CamChannel>("ardrone/setcamchannel");
+
+    // messages for takeoff and landing
+    std_msgs::Empty empty_msg;
+    std_msgs::Twist twist_msg;
+
+    // set camchannel on drone and takeoff
     if(ros::ok()) {
-        std_msgs::Empty takeoff_msg;
-        std_msgs::Empty land_msg;
-        geometry_msgs::Twist twist_msg;
-
-        ROS_INFO("Theoretically, takeoff.\n");
-        takeoff_pub.publish(takeoff_msg);
+        ardrone_autonomy::CamChannel camsrv;
+        camsrv.request.channel = 1;
+        launch.pub(empty_msg);
+    } 
     
-        ros::Duration(15.0).sleep();
-        twist_msg.angular.z = 0.5;
-        twist_pub.publish(twist_msg);
-        ROS_INFO("Theoretically, spinning.\n");
-        ros::Duration(5.0).sleep();
-        twist_msg.angular.z = 0;
-        twist_pub.publish(twist_msg);
-        ROS_INFO("Theoretically, hovering.\n");
-        ros::Duration(5.0).sleep();
-
-        land_pub.publish(land_msg);
-        ROS_INFO("Theoretically, landing.\n");
+    // hover drone in place
+    while(ros::ok()) {
+        ros::spinOnce(); // see CamCallback()
+        loop_rate.sleep();
     }
 
     return 0;
-}*/
+}
