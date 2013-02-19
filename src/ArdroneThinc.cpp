64590ca21d1b5dcf3f4a5a6db17a9eb7007ae76d
dmillard <dmillard10@gmail.com>
2013-02-19 14:12:36 -0500

    hough compiling

--- src/ArdroneThinc.cpp
+++ src/ArdroneThinc.cpp
@@ -24,48 +24,36 @@ using namespace std;
 
 // threshold images and adjust drone position accordingly
 void ArdroneThinc::CamCallback(const sensor_msgs::ImageConstPtr& rosimg) {
-    cv_bridge::CvImageConstPtr cvimg = cv_bridge::toCvShare(rosimg);
-    cv_bridge::CvImagePtr cvimgsmall(new cv_bridge::CvImage()); 
     // convert ros image to opencv image
-    cv_bridge::CvImagePtr cvimghsv(new cv_bridge::CvImage()); 
-    cv_bridge::CvImagePtr cvimgthresh(new cv_bridge::CvImage()); 
-    resize(cvimg->image, cvimgsmall->image, Size(), .25, .25);
-    cvtColor(cvimgsmall->image, cvimghsv->image, CV_RGB2HSV); // RGB->HSV
-    blur(cvimghsv->image, cvimghsv->image, Size(2, 2)); // remove noise
-    // threshold image
-    inRange(cvimghsv->image,
-            Scalar(140, 0, 200),
-            Scalar(180, 255, 255),
-            cvimgthresh->image);
-    // calculate center of white pixels in thresholded image
-    Moments m = moments(cvimgthresh->image);
-    int imgw = cvimgthresh->image.size.p[1];
-    int imgh = cvimgthresh->image.size.p[0];
-    double x = m.m10/m.m00;
-    double y = m.m01/m.m00;
-    double xp = x/imgw;
-    double yp = y/imgh;
-    // mark center of threshold and image, and draw boundary lines
-    Scalar mc(128); // marking color (mono, anyway)
-    circle(cvimgthresh->image, Point(x, y), 1, mc, -1);                   
-    Point center(imgw/2, imgh/2);
-    circle(cvimgthresh->image, center, 1, mc, -1);                   
-    line(cvimgthresh->image, Point(0, imgh*LB), Point(imgw, imgh*LB), mc);
-    line(cvimgthresh->image, Point(0, imgh*UB), Point(imgw, imgh*UB), mc);
-    line(cvimgthresh->image, Point(imgw*LB, 0), Point(imgw*LB, imgh), mc);
-    line(cvimgthresh->image, Point(imgw*UB, 0), Point(imgw*UB, imgh), mc);
+    cv_bridge::CvImagePtr orig = cv_bridge::toCvCopy(rosimg);
+    cv_bridge::CvImagePtr grey(new cv_bridge::CvImage()); 
+
+    // resize image (faster processing);
+    int new_w = orig->image.size.p[0];
+    int new_h = (new_w*orig->image.size.p[1])/orig->image.size.p[0];
+    resize(orig->image, orig->image, Size(new_w, new_h), 0, 0);
+
+    // detect circles using hough transform
+    cvtColor(orig->image, grey->image, CV_RGB2GRAY); // rgb -> grey
+    GaussianBlur(grey->image, grey->image, Size(2, 2), 0); // denoise
+    vector<Vec3f> c;
+    HoughCircles(grey->image, c, CV_HOUGH_GRADIENT, 2, 0, 200, 100);
+    for(size_t i = 0; i < c.size(); i++) {
+        Point center(cvRound(c[i][0]), cvRound(c[i][1]));
+        int radius = cvRound(c[i][2]);
+        circle(orig->image, center, 3, Scalar(0, 255, 0), -1, 8, 0);
+        circle(orig->image, center, radius, Scalar(255, 0, 0), 3, 8, 0);
+    }
 
     // convert opencv image to ros image and publish
-    cvimgthresh->encoding = "mono8";
-    sensor_msgs::ImagePtr rosimgthresh = cvimgthresh->toImageMsg();
-    thresh.publish(rosimgthresh);
+    thresh.publish(orig->toImageMsg());
     
     // center drone
-    if(xp > UB) twist_msg.linear.y = -VEL;
-    else if(xp < LB) twist_msg.linear.y = VEL;
-    else if(LB < xp && xp < UB) twist_msg.linear.y = 0;
-    if(yp > UB) twist_msg.linear.x = -VEL;
-    else if(yp < LB) twist_msg.linear.x = VEL;
-    else if(LB < yp && yp < UB) twist_msg.linear.x = 0;
+    //if(xp > UB) twist_msg.linear.y = -VEL;
+    //else if(xp < LB) twist_msg.linear.y = VEL;
+    //else if(LB < xp && xp < UB) twist_msg.linear.y = 0;
+    //if(yp > UB) twist_msg.linear.x = -VEL;
+    //else if(yp < LB) twist_msg.linear.x = VEL;
+    //else if(LB < yp && yp < UB) twist_msg.linear.x = 0;
     //twist.publish(twist_msg);
 }
