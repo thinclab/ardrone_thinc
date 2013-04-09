97f63654a91704007ad13ac8f19c1f4bcaef9aa9
Emily <ewall@uga.edu>
2013-04-09 14:48:26 -0400

    socket changes

--- src/Socket.cpp
+++ src/Socket.cpp
@@ -1,12 +1,30 @@
+//ros
+#include "ros/ros.h"
+#include "std_msgs/Empty.h"
+#include "std_msgs/Header.h"
+#include "geometry_msgs/Twist.h"
+#include "ardrone_autonomy/Navdata.h"
+
+
+//sockets
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <string.h>
-#include "Socket.hpp"
 
-Socket::Socket(void) {
-   
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+int main(int argc, char**argv) {
+    ros::init(argc, argv, "rocket_sockets");
+    ros::NodeHandle nh;
+    ros::Rate loop_rate(10);
+
+    int sockfd;
+    int n;
+    struct sockaddr_in servaddr;
+    struct sockaddr_in cliaddr;
+    socklen_t len;
+    char mesg[1000];
+
+    sockfd=socket(AF_INET, SOCK_DGRAM, 0);
 
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
@@ -18,11 +36,12 @@ Socket::Socket(void) {
         len = sizeof(cliaddr);
         n = recvfrom(sockfd, mesg, 1000, 0, (struct sockaddr*)&cliaddr, &len);
         sendto(sockfd, mesg, n, 0, (struct sockaddr*)&cliaddr, sizeof(cliaddr));
-        //send back boolean to determine if we are over the other drone
         printf("-------------------------------------------------------\n");
         mesg[n] = 0;
         printf("Received the following:\n");
         printf("%s", mesg);
         printf("-------------------------------------------------------\n");
     }
+
+    return 0; 
 }
